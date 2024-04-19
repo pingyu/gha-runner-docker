f7123ccc8fe69945f664337484b3cf84f358b759
Ping Yu <yuping@pingcap.com>
2024-04-19 20:39:34 +0800

    no network host
    Signed-off-by: Ping Yu <yuping@pingcap.com>

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 echo "Github Runner for $(hostname)"
 
 docker build -t gha-runner .
-docker run -ti --network=host --name=gha-runner-$1-$2 -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock gha-runner $1 $2 $3 "$(hostname)"
+docker run -ti --name=gha-runner-$1-$2 -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock gha-runner $1 $2 $3 "$(hostname)"
