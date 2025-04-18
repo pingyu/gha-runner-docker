a3b91a5e8eb455b5a97798c46c0559e407b26b90
Ping Yu <yuping@pingcap.com>
2025-04-18 23:09:18 +0800

    add run cse
    Signed-off-by: Ping Yu <yuping@pingcap.com>

---
+++ run-cse.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Github Runner for CSE on $(hostname)"
+
+TYPE="*"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -t | --token)
+        shift
+        TOKEN="$1"
+        ;;
+    --type)
+        shift
+        TYPE="$1"
+        ;;
+    *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+if [[ -z "${TOKEN:-}" ]]; then
+    echo "Usage: $0 --token <token> --type <*/ci/cd>"
+    exit 1
+fi
+
+if [[ "${TYPE}" == "ci" || "${TYPE}" == "*" ]]; then
+    ./run.sh --owner tidbcloud --repo cloud-storage-engine --token "$TOKEN" --type ci
+elif [[ "${TYPE}" == "cd" || "${TYPE}" == "*" ]]; then
+    ./run.sh --owner tidbcloud --repo cloud-storage-engine --token "$TOKEN" --type cd
+else
+    echo "Unknown type: $TYPE"
+    exit 1
+fi
