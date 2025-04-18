ddf309ea1693a204a29a2ae6a6ffefa945db5bfc
Ping Yu <yuping@pingcap.com>
2025-04-18 22:51:39 +0800

    support type for ci/cd
    Signed-off-by: Ping Yu <yuping@pingcap.com>

--- run.sh
+++ run.sh
@@ -4,5 +4,44 @@ set -euo pipefail
 
 echo "Github Runner for $(hostname)"
 
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -o | --owner)
+        shift
+        OWNER="$1"
+        ;;
+    -r | --repo)
+        shift
+        REPO="$1"
+        ;;
+    -t | --token)
+        shift
+        TOKEN="$1"
+        ;;
+    --type)
+        shift
+        TYPE="$1"
+        ;;
+    *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+if [[ -z "${OWNER:-}" || -z "${REPO:-}" || -z "${TOKEN:-}" ]]; then
+    echo "Usage: $0 --owner <owner> --repo <repo> --token <token> --type <ci/cd>"
+    exit 1
+fi
+
+TYPE="${TYPE:-}"
+CONTAINER_NAME="gha-runner-$OWNER-$REPO"
+if [[ -n "$TYPE" ]]; then
+    CONTAINER_NAME="$CONTAINER_NAME-$TYPE"
+fi
+
 docker build -t gha-runner .
-docker run -ti --name=gha-runner-$1-$2 -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock gha-runner $1 $2 $3 "$(hostname)"
+docker run -ti --name="$CONTAINER_NAME" -d --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock gha-runner \
+    --owner "$OWNER" --repo "$REPO" --token "$TOKEN" --type "$TYPE" --hostname "$(hostname)"
--- start.sh
+++ start.sh
@@ -2,16 +2,56 @@
 
 cd /home/docker/actions-runner || exit 1
 
-ARCH=`dpkg --print-architecture`
+ARCH=$(dpkg --print-architecture)
+LABELS="linux,${ARCH}"
 
-./config.sh --url https://github.com/$1/$2 --token $3 --name runner-${ARCH}-"$4" --replace --labels linux,${ARCH} --unattended
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -o | --owner)
+        shift
+        OWNER="$1"
+        ;;
+    -r | --repo)
+        shift
+        REPO="$1"
+        ;;
+    -t | --token)
+        shift
+        TOKEN="$1"
+        ;;
+    --type)
+        shift
+        TYPE="$1"
+        if [[ -n "$TYPE" ]]; then
+            LABELS="$LABELS,$TYPE"
+        fi
+        ;;
+    -h | --hostname)
+        shift
+        HOSTNAME="$1"
+        ;;
+    *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+RUNNER_NAME="runner-$ARCH-$HOSTNAME"
+if [[ -n "${TYPE:-}" ]]; then
+    RUNNER_NAME="$RUNNER_NAME-$TYPE"
+fi
+
+./config.sh --url https://github.com/"$OWNER"/"$REPO" --token "$TOKEN" --name "$RUNNER_NAME" --replace --labels "$LABELS" --unattended
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --token ${REG_TOKEN}
+    ./config.sh remove --token "$TOKEN"
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+./run.sh &
+wait $!
